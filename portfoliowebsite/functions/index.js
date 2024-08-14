e831ffca92fe7cd4225716e43f4f0c070105f751
Omar Osmanovic <osmanovicomar@gmai.com>
2024-08-14 14:10:15 +0200

    Minor fix

--- portfoliowebsite/functions/index.js
+++ portfoliowebsite/functions/index.js
@@ -2,7 +2,7 @@ const functions = require("firebase-functions");
 const axios = require("axios");
 
 exports.sendContactEmail = functions.https.onRequest(async (req, res) => {
-  const {fullName, email, inquiry} = req.body;
+  const { fullName, email, inquiry } = req.body;
 
   // Validate the input
   if (!fullName || !email || !inquiry) {
@@ -10,34 +10,31 @@ exports.sendContactEmail = functions.https.onRequest(async (req, res) => {
   }
 
   // Set up Brevo API credentials and endpoint
-  const apiKey =
-    "xkeysib-2c25c3eaab259caea328d" +
-    "7e5bb3a08cbfba83c21b32c2d5ad40" +
-    "4103ec54027f4-zeGH49WUpxy06d7I";
+  const apiKey = "nothing to see here, move along";
   const apiEndpoint = "https://api.brevo.com/v3/smtp/email";
 
   try {
     await axios.post(
       apiEndpoint,
       {
-        sender: {email: email, name: fullName},
+        sender: { email: email, name: fullName },
         to: [
           {
             email: "osmanovicomar@gmail.com",
-            name: "Omar Osmanovic"
-          }
+            name: "Omar Osmanovic",
+          },
         ],
         subject: "Omar Osmanovic Website Inquiry",
         htmlContent: `
         <p>Name: ${fullName}</p>
         <p>Email: ${email}</p>
-        <p>Inquiry: ${inquiry}</p>`
+        <p>Inquiry: ${inquiry}</p>`,
       },
       {
         headers: {
           "Content-Type": "application/json",
-          "api-key": apiKey
-        }
+          "api-key": apiKey,
+        },
       }
     );
 
